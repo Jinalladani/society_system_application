b55bee0ab6dde722d6f3861c30eb6e45ce529b40
Dhruvanshu1775 <dhruvanshu1775@gmail.com>
2021-12-11 13:10:01 +0530

    update user detail and society model

---
+++ myapp/check_me.py
@@ -0,0 +1,12 @@
+from django.shortcuts import redirect
+
+def check_user(function):
+	def wrapper(request, *args, **kwargs):
+		user = request.user 
+		if not user.is_authenticated:
+			return redirect('loginpage')
+		elif user.is_authenticated:
+			return function(request, *args, **kwargs)
+
+		return function(request, *args, **kwargs)
+	return wrapper		
