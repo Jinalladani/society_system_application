747c021091f1cb7e2d24ff490ee847c3dd2224d7
jinalladani25 <50820489+jinalladani25@users.noreply.github.com>
2021-12-31 04:16:05 -0800

    management

--- userinterface/settings.py
+++ userinterface/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'userinterface.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Society_Management_Application',
-        'USER': 'jinal',
-        'PASSWORD': 'jinal123',
-        'HOST': '64.227.186.69',
+        'NAME': 'Society_Management_System',
+        'USER': 'root',
+        'PASSWORD': 'myr00t',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
